b28d2e8b291ff5c1ce9b06d76f7b2dadebd6943b
shrestha3103 <shresthakundu5@gmail.com>
2026-02-01 22:24:59 +0530

    Content of About Updated

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,19 +1,22 @@
 export default function About() {
   return (
     <section id="about" className="py-27 bg-(--offwhite)">
-      <div className="max-w-6xl min-h-screen mx-auto px-6 flex flex-col gap-20">
-        <h2 className="text-2xl font-semibold text-(--maroon) mb-4">About Us</h2>
-        <div className="text-gray-800">
-          Peers Symantech is a multi-disciplinary, Technology driven and customer focused organization into the business of Software/App development and IT Enabled Services with leading edge engineering solution.
-        </div>
-        <div>
-          We have worked in various verticals in Software Development. However, the specialization that put us stand apart is Medical Informatics. We have number of software developed in this Vertical. Software, we have developed in this field has helped Medical and allied professionals access and utilize various advanced techniques in Medical Informatics like Electronic Medical Record (EMR) and Clinical Decision Support System(CDSS)
-        </div>
-        <div>
-          We have received award from: 
-        </div>
-        <div>
-          We are incubated in Nexus, US Embassy, New Delhi.
+      <div className="max-w-6xl min-h-screen mx-auto px-6">
+        <h2 className="text-2xl font-semibold text-(--maroon) mb-25">About Us</h2>
+        <div className="flex flex-col gap-20">
+
+          <div className="">
+            Peers Symantech is a multi-disciplinary, Technology driven and customer focused organization into the business of Software/App development and IT Enabled Services with leading edge engineering solution.
+          </div>
+          <div>
+            We have worked in various verticals in Software Development. However, the specialization that put us stand apart is Medical Informatics. We have number of software developed in this Vertical. Software, we have developed in this field has helped Medical and allied professionals access and utilize various advanced techniques in Medical Informatics like Electronic Medical Record (EMR) and Clinical Decision Support System(CDSS).
+          </div>
+          <div>
+            We have received award from:
+          </div>
+          <div>
+            We are incubated in Nexus, US Embassy, New Delhi.
+          </div>
         </div>
       </div>
     </section>
